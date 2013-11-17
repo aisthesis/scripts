8394b6b6c49deea44a3a50c1acb35a998694d696
Marshall Farrier <info@marshallfarrier.com>
2013-11-17 03:39:28 -0800

    Improved implementation of inheritance.

---
+++ tst/extend-test.js
@@ -0,0 +1,32 @@
+var Person = Base.extend({
+    init: function(isDancing) {
+        this.dancing = isDancing;
+    },
+    dance: function() {
+        return this.dancing;
+    }
+});
+
+var Ninja = Person.extend({
+    init: function() {
+        this._super.init(false);
+    },
+    dance: function() {
+        return this._super.dance();
+    },
+    swingSword: function() {
+        return true;
+    }
+});
+
+test( "inheritance test", function() {
+    var person = new Person(true);
+    ok(person.dance(), "person is dancing");
+    var ninja = new Ninja();
+    ok(ninja.swingSword(), "sword swinging");
+    ok(!ninja.dance(), "ninja isn't dancing");
+    ok(person instanceof Base, "person instance of Base");
+    ok(person instanceof Person, "person instance of Person");
+    ok(ninja instanceof Ninja, "ninja instance of Ninja");
+    ok(ninja instanceof Person, "ninja instance of Person");
+});
